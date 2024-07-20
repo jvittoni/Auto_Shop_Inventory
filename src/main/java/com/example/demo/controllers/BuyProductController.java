8a49de12d7344a5291fba183458228f27b5f4362
Javae Vittone <jvitton@wgu.edu>
2024-07-20 11:51:39 -0700

    File Name: BuyProductController.java / Line: 1 - 44 / Edit: Created file BuyProductController.java and added a controller for the "Buy Now" button and added imports

---
+++ src/main/java/com/example/demo/controllers/BuyProductController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import com.example.demo.domain.Part;
+import com.example.demo.domain.Product;
+import com.example.demo.repositories.ProductRepository;
+import com.example.demo.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class BuyProductController {
+    @Autowired private ProductRepository productRepository;
+
+    @GetMapping("/buyProduct")
+    public String buyProduct(@RequestParam("productID") Long theld, Model theModel) {
+        Optional<Product> productToBuy = productRepository.findById(theld);
+
+        if (productToBuy.isPresent()) {
+            Product product = productToBuy.get();
+
+            if (product.getInv() > 0) {
+                product.setInv(product.getInv() - 1);
+                productRepository.save(product);
+
+                return "/confirmbuysuccess";
+            } else {
+                return "/confirmbuyfail";
+            }
+        } else {
+            return "/confirmbuyfail";
+        }
+    }
+}
